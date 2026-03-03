9b59174602c5fc2c7b10c5f3230d26373bd0a32b
Claudio Matheus <claudio1.code@gmail.com>
2026-03-03 16:39:14 -0300

    feat: implementar classe Zombie e funções auxiliares

--- ex00/Zombie.hpp
+++ ex00/Zombie.hpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: clados-s <clados-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/03 15:43:03 by clados-s          #+#    #+#             */
+/*   Updated: 2026/03/03 15:43:06 by clados-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef ZOMBIE_HPP
 #define ZOMBIE_HPP
 #include <iostream>
@@ -7,12 +19,14 @@ class Zombie
 	private:
 		std::string name;
 	public:
+		Zombie();
 		Zombie(std::string name);
-		~Zombie();
 		void announce(void);
+		void setName(std::string name);
+		~Zombie();
 };
 
-Zombie*	newZombie( std::string name );
-void	randomChump( std::string name );
+Zombie*	newZombie(std::string name);
+void	randomChump(std::string name);
 
 #endif
